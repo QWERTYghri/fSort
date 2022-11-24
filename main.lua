adb507725408d6e6671b129babe19ea1540a3b86
QWERTYbae <nitwitghfj17717@protonmail.com>
2022-11-23 20:54:59 -0800

    Added error is no input is given

--- main.lua
+++ main.lua
@@ -19,6 +19,24 @@ local input     = {}
 local target    = "./" -- Default
 local openP     = "" -- Default
 
+local msgList = 
+{
+        pathNonExist = "Error: path doesn't exist || mangled args\nExiting...\n\n",
+        flagNotKnown = "Error: flag not known: \"%s\"\nExiting...\n\n",
+        helpInfo =
+-- CRINGE
+[[
+Usage
+----------
+* --input [ARGS...]   : Select what files you want to sort out; if no file exists then it'll error
+* --tdir  [ARG]       : The target directory you want to sort to, ( if none exist then it'll create a new one )
+* --open  [ARG]       : It'll open the file with a specified program if you specify to open it
+* --help              : Gives help...
+
+@QWERTYghri fucking help me
+]]
+}
+
 local funcList =
 {
         ["--input"] = 
@@ -38,25 +56,34 @@ local funcList =
                 if ( lfs.attributes ( args ) ) then
                         openP = args
                 else
-                        errMsg ( string.format ( "Error: path doesn't exist || mangled args\nExiting...\n\n" ) )
+                        errMsg ( string.format ( msgList.pathNonExist ) )
                 end
 
                 return argPos + 2
+        end,
+        ["--help"] =
+        function ( argPos )
+
         end
 }
 
 -- Functions
 local function argParse ()
+        if ( #arg == 0 ) then
+                errMsg ( string.format ( "No input is given...\n\n%s",  msgList.helpInfo ) )
+        end
+        
         local i = 1
 
         while ( i <= #arg ) do --pairs won't do good here, Also seems like for loops are much more references that actual variables. so fuck this
                 if ( funcList[arg[i]] ~= nil ) then
-                        i = funcList[arg[i]] ( i )
+                        i = funcList[arg[i]] ( i ) <= #arg
                 else
-                        errMsg ( string.format ( "Error: flag not known: \"%s\"\nExiting...\n\n", arg[i] ) )
+                        errMsg ( string.format ( msgList.flagNotKnown, arg[i] ) )
                 end
                 i = i + 1
         end
+
         os.execute ( openP )
 end
 
